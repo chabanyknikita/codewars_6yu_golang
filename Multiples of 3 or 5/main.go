1afbb0d476681c175f2824acd0bfb36c57726f8a
chabanyknikita <nikchabanyk@gmail.com>
2022-10-27 13:07:07 +0300

    6kyu

---
+++ Multiples of 3 or 5/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func Multiple3And5(number int) int {
+	var new_arr []int
+	summary := 0
+	for i := 1; i < number; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			new_arr = append(new_arr, i)
+		}
+	}
+	for _, i := range new_arr {
+		summary += i
+	}
+	return summary
+}
+
+func main() {
+	fmt.Println(Multiple3And5(10))
+}
